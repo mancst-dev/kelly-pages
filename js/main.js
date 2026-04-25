a077b3cb63435c60a32362860a9803229a110762
Manuel Castaño <140443578+mancst-dev@users.noreply.github.com>
2026-04-25 18:24:35 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,3 @@
-/*
-	Hyperspace by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
-
 (function($) {
 
 	var	$window = $(window),
@@ -137,13 +131,13 @@
 				bottom: '-10vh',
 				initialize: function() {
 
-					// Deactivate section.
+					//Deactivate section.
 						$(this).addClass('inactive');
 
 				},
 				enter: function() {
 
-					// Activate section.
+					//activate section.
 						$(this).removeClass('inactive');
 
 				}
